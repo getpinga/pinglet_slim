afdbad60a1ea2c072f274402d8bc13933148f965
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-21 12:29:45 +0200

    Updated routes demo with more examples

--- app/routes.php
+++ app/routes.php
@@ -1,25 +1,35 @@
 <?php
 
-/**
- * Application routes list.
- *
- * @link https://www.slimframework.com/docs/v4/objects/routing.html
- */
-
 declare(strict_types=1);
 
 use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 use Psr\Http\Message\ServerRequestInterface as PiRequest;
 use Psr\Http\Message\ResponseInterface as PiResponse;
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
 
 return function (App $app): void {
+    $app->options('/{routes:.*}', function (PiRequest $request, PiResponse $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
 
-    $app->get('/', fn () => json(['status' => 'ok']));
-	
-	$app->get('/hello/{name}', function (PiRequest $request, PiResponse $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
+    $app->get('/', function (PiRequest $request, PiResponse $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
 
+    $app->get('/json', fn () => json(['status' => 'ok']));
+
+    $app->get('/hello/{name}', function (PiRequest $request, PiResponse $response, array $args) {
+        $name = $args['name'];
+        $response->getBody()->write("Hello, $name");
+        return $response;
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
 };
