e9767833cf0ef1e884458b3ef8c7d966b2bfaf4a
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-23 23:54:58 +0200

    Update index.php

--- routes/index.php
+++ routes/index.php
@@ -11,8 +11,6 @@ use App\Application\Actions\User\ViewUserAction;
 use Kodus\Cache\FileCache;
 use function Compwright\PhpSession\Frameworks\Slim\registerSessionMiddleware;
 
-require_once dirname(__DIR__) . '/app/session.php';
-
 return function (App $app): void {
 	$sessionFactory = new Compwright\PhpSession\Factory();
 	$manager = $sessionFactory->psr16Session(
@@ -30,10 +28,10 @@ return function (App $app): void {
 		throw new RuntimeException("The session failed to start");
 	}
 
-/*     $app->options('/{routes:.*}', function (PiRequest $request, PiResponse $response) use ($manager) {
+     $app->options('/{routes:.*}', function (PiRequest $request, PiResponse $response) use ($manager) {
         // CORS Pre-Flight OPTIONS Request Handler
         return $response;
-    }); */
+    }); 
 
     $app->get('/', function (PiRequest $request, PiResponse $response) use ($manager) {
 		
@@ -42,11 +40,9 @@ return function (App $app): void {
 
         $response->getBody()->write('Hello world!');
         return $response;
-    }, [Routes\SessionRoutes::class, 'readSession']);
+    });
 
-    $app->post('/', [Routes\SessionRoutes::class, 'writeSession']);
-
-    $app->get('/json', fn () => json(['status' => 'ok']), [Routes\SessionRoutes::class, 'readSession']);
+    $app->get('/json', fn () => json(['status' => 'ok']));
 
     $app->get('/hello/{name}', function (PiRequest $request, PiResponse $response, array $args) use ($manager) {
 		$session = $manager->getCurrentSession();
@@ -54,10 +50,10 @@ return function (App $app): void {
         $name = $args['name'];
         $response->getBody()->write("Hello, $name");
         return $response;
-    }, [Routes\SessionRoutes::class, 'readSession'],);
+    });
 
     $app->group('/users', function (Group $group) use ($manager) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
-    }, [Routes\SessionRoutes::class, 'readSession']);
+    });
 };
